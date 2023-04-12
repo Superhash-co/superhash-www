cf9600fef9a06bf5ef301a28e4b882b78b9e9e26
konradkiss <konrad@konradkiss.com>
2023-04-12 19:04:52 +0200

    Fixing github pages before final site

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/superhash-www/' : '',
   reactStrictMode: true,
   images:{
     unoptimized: true
